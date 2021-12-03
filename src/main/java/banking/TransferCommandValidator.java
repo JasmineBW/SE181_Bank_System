f74a29dfdf43cea4242a8156aa52fe1bbac1ddc9
Jasmine Ben-Whyte <benwhytejasmine@gmail.com>
2021-12-03 06:47:31 -0500

    added the Transfer command, it's processor, validator and tests. Corrected some errors in the other files.

---
+++ src/main/java/banking/TransferCommandValidator.java
@@ -0,0 +1,43 @@
+package banking;
+
+import java.util.Objects;
+
+public class TransferCommandValidator extends Validator {
+    boolean withdrawValidity, depositValidity;
+    WithdrawCommandValidator withdrawCommandValidator;
+    DepositCommandValidator depositCommandValidator;
+
+    public TransferCommandValidator(Bank bank) {
+        this.bank = bank;
+        withdrawCommandValidator = new WithdrawCommandValidator(bank);
+        depositCommandValidator = new DepositCommandValidator(bank);
+    }
+
+    public boolean validate(String command, String idFrom, String idTo, String amount, String extra) {
+        if (!commandChecker(command) || !idChecker(idFrom) || !idChecker(idTo) || !hasAccountWithIdCheck(idFrom) ||
+                !hasAccountWithIdCheck(idTo) || !Objects.equals(extra, "") || Objects.equals(idTo, idFrom)) {
+
+
+            System.out.println(commandChecker(command) + " " + idChecker(idFrom) + idChecker(idTo) + " " + hasAccountWithIdCheck(idFrom) +
+                    " " + hasAccountWithIdCheck(idTo) + Objects.equals(extra, "") + Objects.equals(idTo, idFrom));
+            return false;
+
+        } else {
+            int integerIdFrom = Integer.valueOf(idFrom), integerIdTo = Integer.valueOf(idTo);
+
+            if (Objects.equals(bank.getAccountType(integerIdFrom), "cd") ||
+                    Objects.equals(bank.getAccountType(integerIdTo), "cd")) {
+                return false;
+            }
+
+            withdrawValidity = withdrawCommandValidator.validate(command, idFrom, amount, extra);
+            depositValidity = depositCommandValidator.validate(command, idTo, amount, extra);
+
+            if (withdrawValidity && depositValidity) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
